d5d53e520dbf224ec1f5a29804907561a82ea0c7
Joel <joelthousend@gmail.com || eyoelseleshizeyohannes@gmail.com>
2023-10-29 23:39:57 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Chart from "./components/Chart";
+import { Link, NavLink, Route, Routes } from "react-router-dom";
+import AllProducts from "./components/AllProducts";
 
 let App = () => {
   let chartContainerRef = useRef();
